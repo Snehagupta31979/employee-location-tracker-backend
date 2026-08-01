bdad80aae0f88cba2875b828e0970b97efe51d39
Snehagupta31979 <sg1928904@gmail.com>
2026-08-01 16:21:17 +0530

    Update EmployeeLocationTrackerApplication.java

--- src/main/java/com/employeetracker/EmployeeLocationTrackerApplication.java
+++ src/main/java/com/employeetracker/EmployeeLocationTrackerApplication.java
@@ -1,17 +1,17 @@
 package com.employeetracker;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
 import com.employeetracker.config.TrackingProperties;
-
+import java.util.TimeZone;
 @SpringBootApplication
 @EnableScheduling
 @EnableConfigurationProperties(TrackingProperties.class)
 public class EmployeeLocationTrackerApplication {
-
+    static {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+    }
     public static void main(String[] args) {
         SpringApplication.run(EmployeeLocationTrackerApplication.class, args);
     }
